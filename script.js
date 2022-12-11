23ff87e5b60c9e443d9567759fa5c2bbf1fc92d6
ifazo <ziaulkarimifaz@gmail.com>
2022-12-11 18:54:39 +0600

    last commit

--- script.js
+++ script.js
@@ -11,3 +11,11 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
